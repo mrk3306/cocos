a4fdbaf78344a71f47ddba1fc11eed2efd125e2d
murakami_saori <sao-murakami@mediba.jp>
2014-08-10 23:44:34 +0900

    サンプル修正

--- HelloWorldApp/Classes/Sprite/Ally/AllyClassLoader.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HelloWorldApp__AllyClassLoader__
-#define __HelloWorldApp__AllyClassLoader__
-
-#include "Ally.h"
-#include "NormalBullet.h"
-#include "Bomb.h"
-
-#endif /* defined(__HelloWorldApp__AllyClassLoader__) */
